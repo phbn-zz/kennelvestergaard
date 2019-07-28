aa5f8ce98c0c754488a467b3cb0929968af345ac
phbn <peterherborgbay@gmail.com>
2019-07-28 19:00:51 +0200

    initial commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/home/phbr/Dropbox/coding/kennelvestergaard/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,10 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/home/phbr/Dropbox/coding/kennelvestergaard/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/home/phbr/Dropbox/coding/kennelvestergaard/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/home/phbr/Dropbox/coding/kennelvestergaard/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("/home/phbr/Dropbox/coding/kennelvestergaard/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+}
+
