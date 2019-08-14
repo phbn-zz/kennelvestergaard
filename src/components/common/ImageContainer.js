742feef67e9bd8e62cdb2fab28bc480cdcb85a66
phbn <peterherborgbay@gmail.com>
2019-08-14 17:53:19 +0200

    Fixed imagecontainer

--- src/components/common/ImageContainer.js
+++ src/components/common/ImageContainer.js
@@ -30,8 +30,10 @@ class ImageContainer extends React.Component {
           onEscapeKeydown={this.handleCloseModal}
         >
           <ModalContent>
-            <Img fluid={this.props.fluid} />
-            <button onClick={this.handleCloseModal}>Close me</button>
+            <ImageRenderModal fluid={this.props.fluid} />
+            <StyledButton onClick={this.handleCloseModal}>
+              Close me
+            </StyledButton>
           </ModalContent>
         </StyledModal>
       </ImageBox>
@@ -43,6 +45,10 @@ const ModalContent = styled.div`
   margin: 15px;
   max-width: 700px;
   width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `;
 
 const StyledModal = Modal.styled`
@@ -57,9 +63,36 @@ const ImageBox = styled.figure`
   width: 100%;
 `;
 
+const ImageRenderModal = styled(Img)`
+  border-radius: 20px;
+  box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.16);
+  min-width: 500px;
+  margin: 20px;
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    min-width: 400px;
+  }
+`;
+
 const ImageRender = styled(Img)`
   border-radius: 20px;
   box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.16);
+`;
+
+const StyledButton = styled.button`
+  cursor: pointer;
+  background-color: palevioletred;
+  font-size: 16px;
+  border-radius: 3px;
+  color: black;
+  border: 2px solid palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  transition: 0.5s all ease-out;
+
+  &:hover {
+    color: white;
+  }
 `;
 
 export default ImageContainer;
