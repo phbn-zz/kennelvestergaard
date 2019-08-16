55f5da023ed1163e35b75a0c9350ed6783bade0c
phbn <peterherborgbay@gmail.com>
2019-08-16 10:07:38 +0200

    Added text to terms

--- src/components/sections/Terms.js
+++ src/components/sections/Terms.js
@@ -19,7 +19,10 @@ const Terms = () => (
         <p>
           Vi benytter Dansk Kennel Klub’s købsaftale. I forbindelse med
           reservation af hvalp indbetales et depositum på 2.500 kr. Fortryder
-          man imod forventning reservationen, tilbagebetales 2.000 kr.<br />
+          man imod forventning reservationen, tilbagebetales 2.000 kr.
+          <br />
+          Indbetaling til konto i Sparekassen Sjælland -Fyn reg nr 9886 - konto
+          nr. 0000345267 med angivelse af navn på hvalp. <br />
           <StyledHyperlink href="https://www.dkk.dk/uploads/documents/Om-DKK/Blanketter/købsaftale.pdf">
             Dansk Kennel Klub's købsaftale kan læses her.
           </StyledHyperlink>
